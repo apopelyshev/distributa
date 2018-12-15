c1fc85c17ab82491d83a0c1caf45b75bba9228fa
Artem Popelyshev <apopelyshev@gmail.com>
2018-12-15 11:46:26 +0100

    Still debugging

--- src/main/java/pl/edu/pwr/w8/distributor/Util.java
+++ src/main/java/pl/edu/pwr/w8/distributor/Util.java
@@ -27,9 +27,8 @@ public class Util {
   
   public static void handleTrackCode(HttpServletRequest req, String arg, Person[] searchArr) {
     for (Person smb : searchArr) {
-      System.out.println("OKAY NOW. PARAM: "+arg);
       String temp = smb.getName().toUpperCase()+"_TRACK";
-      System.out.println("CHECKING "+temp);
+      System.out.println(System.getenv(temp).toUpperCase());
       if (arg==System.getenv(temp)) {
         System.out.println("GOT A MATCH. RETRIEVED IP: "+getIP(req));
         System.setProperty(temp, getMAC(getIP(req)));
