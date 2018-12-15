6f71fd42309dd9493a18f69f8402c1bff959a1e9
Artem Popelyshev <apopelyshev@gmail.com>
2018-12-15 15:53:59 +0100

    Fixed some problems with slf4j

--- src/main/java/pl/edu/pwr/w8/distributor/Util.java
+++ src/main/java/pl/edu/pwr/w8/distributor/Util.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.lang.InterruptedException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
