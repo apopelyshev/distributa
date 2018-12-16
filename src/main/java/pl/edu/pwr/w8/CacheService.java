7c93442809d179584aebcdc5295d5c5a1cb3ef21
Artem Popelyshev <apopelyshev@gmail.com>
2018-12-16 15:56:41 +0100

    Added a lot of debug

--- src/main/java/pl/edu/pwr/w8/CacheService.java
+++ src/main/java/pl/edu/pwr/w8/CacheService.java
@@ -71,28 +71,36 @@ public class CacheService {
   
   //Get client by the known hex code
   public Person getMemberByCode(HttpServletRequest req, String codeFromServlet, PersonArr arrOfAllMembers) {
+    System.out.println("Trying to recognize a member by code: "+codeFromServlet);
     for (Person smb : arrOfAllMembers.getArr()) {
       String generatedKey = generateTrackKey(smb);
       Optional<String> valFromCache = Optional.ofNullable(getCached(generatedKey));
       boolean codeMatchesToEnvCode = System.getenv(generatedKey).equals(codeFromServlet);
       
       if (codeMatchesToEnvCode && !valFromCache.isPresent()) {
+        System.out.println("Got a match! This member has not been recorded till now");
+        System.out.println("Member's MAC is: "+Util.getMAC(req));
         setOrDelCached(generatedKey, Util.getMAC(req));
         return smb;
-      } else if (codeMatchesToEnvCode)
+      } else if (codeMatchesToEnvCode) {
+        System.out.println("Got a match! This member has already been recorded actually.");
         return smb;
+      }
     }
     return null;
   }
   
   // Get client if his/her MAC exists in cache
   public Person getMemberByMAC(HttpServletRequest req, PersonArr arrOfAllMembers) {
+    System.out.println("Trying to recognize a member by MAC");
     for (Person smb : arrOfAllMembers.getArr()) {
       String generatedKey = generateTrackKey(smb);
       Optional<String> valFromCache = Optional.ofNullable(getCached(generatedKey));
       
-      if (valFromCache.isPresent() && valFromCache.orElse("").toString().indexOf(Util.getMAC(req)) >= 0)
+      if (valFromCache.isPresent() && valFromCache.orElse("").toString().equals(Util.getMAC(req))) {
+        System.out.println("Got a match! This member's MAC is recorded and is: "+Util.getMAC(req));
         return smb;
+      }
     }
     return null;
   }
@@ -101,6 +109,7 @@ public class CacheService {
   public String handleAllMemberTrackings(PersonArr arrOfAllMembers, String action) {
     StringBuilder sb = new StringBuilder();
     boolean printNeeded = action.equals("print");
+    System.out.println("Checking if something will be printed: "+printNeeded);
     for (Person smb : arrOfAllMembers.getArr()) {
       String generatedKey = generateTrackKey(smb);
       Optional<String> valFromCache = Optional.ofNullable(getCached(generatedKey));
